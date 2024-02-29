2fd6ac172891a040e431070b38c2f7a104f14ee3
Rohit Garg <Rohitgarg157@gmail.com>
2024-02-29 20:02:12 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            var connectionString = builder.Configuration.GetConnectionString("Appdbconnection");
+            
+            builder.Services.AddDbContext<AppDbContext>(options=>options.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString) ));
+
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NAaF1cXmhKYVBpR2Nbe052flRCal1VVAciSV9jS3pTdEdmWXlfdHVRQmBYVQ==");
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline. 
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
